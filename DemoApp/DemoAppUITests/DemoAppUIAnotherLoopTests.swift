a3132a19251a570756d9590eba97d74f674d2e46
eyaly <eyalyovel@gmail.com>
2023-12-19 22:29:31 +0000

    Add test class with one test that run 10 times

---
+++ DemoApp/DemoAppUITests/DemoAppUIAnotherLoopTests.swift
@@ -0,0 +1,115 @@
+//
+//  DemoAppUIAnotherLoopTests.swift
+//  DemoAppUITests
+//
+//  Created by eyalyovel on 19/12/2023.
+//
+
+import XCTest
+
+final class DemoAppUIAnotherLoopTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample1() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(app.staticTexts["Hello, world!"].exists)
+    }
+    
+    func testExample2() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(app.staticTexts["Hello, world!"].exists)
+    }
+    
+    func testExample3() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(app.staticTexts["Hello, world!"].exists)
+    }
+    
+    func testExample4() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(app.staticTexts["Hello, world!"].exists)
+    }
+    
+    func testExample5() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(app.staticTexts["Hello, world!"].exists)
+    }
+    
+    func testExample6() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(app.staticTexts["Hello, world!"].exists)
+    }
+    
+    func testExample7() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(app.staticTexts["Hello, world!"].exists)
+    }
+    
+    func testExample8() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(app.staticTexts["Hello, world!"].exists)
+    }
+    
+    func testExample9() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(app.staticTexts["Hello, world!"].exists)
+    }
+    
+    func testExample10() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(app.staticTexts["Hello, world!"].exists)
+    }
+    
+}
